81ac1e67955e426bf848a80ce0eba5b7120a6645
Blitzschach <156952393+blitzschach@users.noreply.github.com>
2024-02-03 20:02:48 +0100

    remove generic repository

--- distributed/UserManagement/UserManagement.Domain/Repositories/IUserRepository.cs
+++ distributed/UserManagement/UserManagement.Domain/Repositories/IUserRepository.cs
@@ -1,9 +1,14 @@
-﻿using UserManagement.Domain.Core.Interfaces;
-using UserManagement.Domain.Entities;
+﻿using UserManagement.Domain.Entities;
 
 namespace UserManagement.Domain.Repositories;
 
 public interface IUserRepository
-    : IGenericRepository<User>
 {
+    User Add(User entity);
+    
+    Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default);
+    
+    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    
+    void Remove(User entity);
 }
--- distributed/UserManagement/UserManagement.Infrastructure/Repositories/UserRepository.cs
+++ distributed/UserManagement/UserManagement.Infrastructure/Repositories/UserRepository.cs
@@ -1,7 +1,35 @@
-﻿using UserManagement.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Domain.Entities;
 using UserManagement.Domain.Repositories;
 
 namespace UserManagement.Infrastructure.Repositories;
 
-internal sealed class UserRepository(UserManagementContext context)
-    : GenericRepository<User>(context), IUserRepository;
+internal sealed class UserRepository()
+    : IUserRepository
+{
+    private readonly UserManagementContext _dbContext;
+
+    public UserRepository(UserManagementContext context) 
+        : this()
+        => _dbContext = context;
+
+    public User Add(User entity)
+        => _dbContext
+            .Set<User>()
+            .Add(entity).Entity;
+
+    public async Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default)
+        => await _dbContext
+            .Set<User>()
+            .ToListAsync(cancellationToken);
+
+    public async Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        => await _dbContext
+            .Set<User>()
+            .FirstOrDefaultAsync(u => u.Id == id, cancellationToken);
+
+    public void Remove(User entity)
+        => _dbContext
+            .Set<User>()
+            .Remove(entity);
+}
